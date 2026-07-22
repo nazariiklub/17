1c42d992a51fdf722ab2b849bbd8a846e7decf16
Nazarii Klub <155299604+nazariiklub@users.noreply.github.com>
2026-07-22 15:29:38 +0300

    Delete src directory

--- src/core/light.js
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three';
-
-export function createLight() {
-    const light = new THREE.DirectionalLight(0xffffff, 3);
-
-    light.position.set(5, 5, 5);
-
-    return light;
-}
--- src/core/scene.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from 'three';
-
-export function createScene() {
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x000000);
-
-    return scene;
-}
